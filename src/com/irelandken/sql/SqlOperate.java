fdf783c2b0722bdd9369b8e595ac2c8f2e33ac9e
irelandKen <fengxue2026@qq.com>
2013-11-12 01:13:46 +0800

    添加SELECT,DELETE

--- src/com/irelandken/sql/SqlOperate.java
+++ src/com/irelandken/sql/SqlOperate.java
@@ -1,9 +1,11 @@
 package com.irelandken.sql;
 
+import java.util.Collection;
 import java.util.List;
 import java.util.Map;
 
 import org.springframework.dao.DataAccessException;
+import org.springframework.jdbc.core.SqlParameterValue;
 
 /**
  *  
@@ -29,6 +31,24 @@ public interface SqlOperate
 	 */
 	int insert2(String table, Map<String, Object> data);
 
+	/**
+	 * update the table with the data
+	 * @param table
+	 * @param data
+	 * @param where 
+	 * @return affert rows count
+	 */
+	int select(String table,Collection<String> fields,String where);
+	
+	/**
+	 * update the table with the data
+	 * @param table
+	 * @param data
+	 * @param where 
+	 * @return affert rows count
+	 */
+	int select(String table,Collection<String> fields,Map<String,Object> where);
+	
 	
 	/**
 	 * update the table with the data
@@ -49,11 +69,27 @@ public interface SqlOperate
 	int updata(String table,Map<String, Object> data,Map<String,Object> where);
 	
 	/**
-	 * Issue a single SQL update operation (such as an insert, update or delete statement).
-	 * @param sql static SQL to execute
-	 * @return the number of rows affected
+	 * update the table with the data
+	 * @param table
+	 * @param where 
+	 * @return affert rows count
 	 */
-	int update(String sql);
+	int delete(String table, String where);
+	
+	/**
+	 * update the table with the data
+	 * @param table
+	 * @param where 
+	 * @return affert rows count
+	 */
+	int delete(String table, Map<String,Object> where);
+	
+	
+	
+	//-----------------------------------------------
+	//-----以下为通用的SQL操作------------------------------------------
+	//-----------------------------------------------
+	
 	
 	/**
 	 * Execute a query for a result list, given static SQL.
@@ -69,5 +105,43 @@ public interface SqlOperate
 	 * @throws DataAccessException if there is any problem executing the query
 	 * @see #queryForList(String, Object[])
 	 */
-	List<Map<String, Object>> queryForList(String sql) throws DataAccessException;
+	List<Map<String, Object>> query(String sql) throws DataAccessException;
+	
+	/**
+	 * Query given SQL to create a prepared statement from SQL and a
+	 * list of arguments to bind to the query, expecting a result list.
+	 * <p>The results will be mapped to a List (one entry for each row) of
+	 * Maps (one entry for each column, using the column name as the key).
+	 * Each element in the list will be of the form returned by this interface's
+	 * queryForMap() methods.
+	 * @param sql SQL query to execute
+	 * @param args arguments to bind to the query
+	 * (leaving it to the PreparedStatement to guess the corresponding SQL type);
+	 * may also contain {@link SqlParameterValue} objects which indicate not
+	 * only the argument value but also the SQL type and optionally the scale
+	 * @return a List that contains a Map per row
+	 * @throws DataAccessException if the query fails
+	 * @see #queryForList(String)
+	 */
+	List<Map<String, Object>> query(String sql, Object... args) throws DataAccessException;
+
+	/**
+	 * Issue a single SQL update operation (such as an insert, update or delete statement).
+	 * @param sql static SQL to execute
+	 * @return the number of rows affected
+	 */
+	int queryUpdate(String sql);
+	
+	/**
+	 * Issue a single SQL update operation (such as an insert, update or delete statement)
+	 * via a prepared statement, binding the given arguments.
+	 * @param sql SQL containing bind parameters
+	 * @param args arguments to bind to the query
+	 * (leaving it to the PreparedStatement to guess the corresponding SQL type);
+	 * may also contain {@link SqlParameterValue} objects which indicate not
+	 * only the argument value but also the SQL type and optionally the scale
+	 * @return the number of rows affected
+	 */
+	int queryUpdate(String sql, Object... args);
+	
 }
